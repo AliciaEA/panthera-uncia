41375a02a6046c01b29915b0fe3b990dda340f39
AliciaEA <aliestrada@uamv.edu.ni>
2025-08-15 15:20:19 -0600

    lets try

--- panthera-uncia/svelte.config.js
+++ panthera-uncia/svelte.config.js
@@ -3,10 +3,19 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+
+		adapter: adapter({
+
+			pages: 'build',      // directory where pages will be outputted
+			assets: 'build',     // directory where assets will be outputted
+			fallback: undefined, // for single-page applications, specifies a fallback page
+			precompress: false,  // whether to compress files
+			strict: true         // whether to fail if the build is not strict
+		}),
+		paths: {
+
+			base: process.env.NODE_ENV === 'production' ? '/panthera-uncia' : '',
+		}
 	}
 };
 
